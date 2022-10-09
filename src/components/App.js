405dd91f6a6fd16d1a4b26eae295e8b973b3445c
prasadmagi <magiprasad@gmail.com>
2022-10-09 14:51:47 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,27 @@ import '../styles/App.css';
 import { useState, useEffect } from 'react';
 const App = () => {
 //code here 
- 
+ const [name,setname] = useState([]);
+ const [id,setid] = useState(1);
+useEffect(()=>{
+  fetch(`https://content.newtonschool.co/v1/pr/main/users/${id}`)
+  .then(res=>res.json())
+  .then(data=>{
+    console.log(data);
+    setname(data)
+  })
+},[id])
 
-
+const changeInput = (e)=>{
+  
+  setid(e.target.value)
+}
 
   return (
     <div className="App">
       <h1 id="text">Type a number between 1 and 10</h1>
       <input id="input" onChange={changeInput} />
-      <p id="name">{name}</p>
+      <p id="name">{name.name}</p>
     </div>
   );
 }
